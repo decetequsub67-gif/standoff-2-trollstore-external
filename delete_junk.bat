e4dba33a00eb85e6148910240a6d8dbb8f3bd507
tavrida1337-cmyk <tavrida1337@gmail.com>
2026-04-18 17:38:10 +0300

    y2

--- delete_junk.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Cleaning up Zone.Identifier trash files...
-powershell -Command "Get-ChildItem -Path . -Recurse -Include '*Zone*', '*Identifier*' -Exclude 'delete_junk.bat' -ErrorAction SilentlyContinue | Remove-Item -Force"
-echo Done!
-pause
